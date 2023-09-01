6440586233938b5991347dda154f9ec224d10241
cogicgoran <cogicgoran99@gmail.com>
2023-09-01 10:19:16 +0200

    Add binary search, iterative. Use assertion library for testing

--- linear_search.js
+++ linear_search.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 function linearSearch(array, target) {
     for (let index = 0; index < array.length; index++) {
         if (array[index] === target) {
@@ -7,15 +9,11 @@ function linearSearch(array, target) {
     return -1;
 }
 
-function verify(array, target) {
-    const foundIndex = linearSearch(array, target);
-    if (foundIndex === -1) {
-        console.log('Target not found');
-    } else {
-        console.log(`Target found on index '${foundIndex}'`);
-    }
-}
-
-verify([1, 2, 3, 4, 5, 6, 7, 8], 12);
-verify([1, 2, 3, 4, 5, 6, 7, 8], 5);
-verify([1, 2, 3, 4, 5, 6, 7, 8], -22);
+assert.equal(linearSearch([1, 2, 3], 2), 1);
+assert.equal(linearSearch([1, 2, 3], 3), 2);
+assert.equal(linearSearch([1, 2, 3], 5), -1);
+assert.equal(linearSearch([1, 2, 3, 4], 3), 2);
+assert.equal(linearSearch([1, 2, 3, 4], 2), 1);
+assert.equal(linearSearch([1, 2, 3, 4], 5), -1);
+assert.equal(linearSearch([1, 2, 3, 4], 0), -1);
+console.log('Tests passed successfully');
